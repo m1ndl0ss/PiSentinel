5b168a42d25a5d7184860655451ad8c216a63bc3
MatKa <m.kandalintsev@student.maastrichtuniversity.nl>
2026-01-19 17:28:55 +0100

    quick fixes

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from camera import Camera
-from motion import motion_detected
-from faceRecognition import recognize_face
+from motion import Motion
+from faceRecognition import recognise_face
 from database import load_known_faces
 
 def main():
     camera = Camera()
+    motion_detected = Motion()
     known_faces = load_known_faces("data/known_faces")
     try:
         while True:
             frame = camera.capture()
-            if motion_detected(frame):
-                name = recognize_face(frame)
+            if motion_detected.detect(frame):
+                name = recognise_face(frame, known_faces)
                 print(name) #for debug
     finally:
         camera.release()
--- motion.py
+++ motion.py
@@ -1,14 +1,15 @@
 import cv2
 
 class Motion:
-    def __init__(self, threshold =30, blurSensetivity=21):
+    def __init__(self, pixelIntensityTreshold =30, blurSensitivity=21,pixelCountThreshold=25):
         self.previous_frame = None
-        self.threshold = threshold
-        self.blurSensetivity = blurSensetivity
+        self.pixelIntensityThreshold = pixelIntensityTreshold
+        self.blurSensitivity = blurSensitivity
+        self.pixelCountThreshold=pixelCountThreshold
 
     def detect(self, frame):
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #1 gray channel is sufficient
-        gray = cv2.GaussianBlur(gray, (self.blurSensetivity,self.blurSensetivity), 0)
+        gray = cv2.GaussianBlur(gray, (self.blurSensitivity,self.blurSensitivity), 0)
 
         if self.previous_frame is None:
             self.previous_frame = gray
@@ -16,8 +17,8 @@ class Motion:
         #find difference
 
         frame_diff = cv2.absdiff(self.previous_frame, gray)
-        thresh = cv2.threshold(frame_diff, self.threshold, 255, cv2.THRESH_BINARY)[1] #converts the diff into black and white image where white- movement
+        thresh = cv2.threshold(frame_diff, self.pixelIntensityThreshold, 255, cv2.THRESH_BINARY)[1] #converts the diff into black and white image where white- movement
         #self-note : threshbinary sets to max value if pixel exceeds thresh, otherwise to 0, we get black n white
         motion_pixels = cv2.countNonZero(thresh)
 
-        return motion_pixels > self.threshold
+        return motion_pixels >self.pixelCountThreshold
