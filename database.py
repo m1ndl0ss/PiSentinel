58bab59e96ce71741719be0dd7607e5edd911d15
MatKa <m.kandalintsev@student.maastrichtuniversity.nl>
2026-01-19 17:44:55 +0100

    completed database, load known faces. fixed up main. ran into an issue- only 1 face at a time

--- database.py
+++ database.py
@@ -2,9 +2,19 @@ import os
 import faceRecognition
 import face_recognition
 
-def get_known_faces():
+def load_known_faces(directory):
     known_faces = {}
-    for filename in os.listdir('known_faces'):
+    for filename in os.listdir(directory):
         if filename.endswith(('.jpg', '.png')):
             path = os.path.join('known_faces', filename)
-            image = faceRecognition.load_image_file(path)
+            image = face_recognition.load_image_file(path)
+            encoding=face_recognition.face_encodings(image)
+
+            if encoding:
+                name = os.path.splitext(filename)[0]
+                known_faces[name] = encoding[0]
+            else:
+                print(f"Warning : No face found in {filename}")
+    return known_faces
+
+
